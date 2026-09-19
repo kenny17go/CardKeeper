40ee38ce726767c1eae54d92f2ac78c3ec319735
kenny17go <kenny0217@ymail.com>
2026-09-19 15:36:53 +0800

    v5.1.2: refresh service worker cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* CardKeeper v5.1 OCR service worker */
-const CACHE_NAME = 'cardkeeper-shell-v5.1.1';
+const CACHE_NAME = 'cardkeeper-shell-v5.1.2';
 const SHELL_FILES = [
   './','./index.html','./css/style.css','./js/db.js','./js/parse.js',
   './js/vision.js','./js/ocr.js','./js/vcard.js','./js/camera.js','./js/app.js',
